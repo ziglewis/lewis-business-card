cdf3990f959080e298022e6cc51a383f9d349d1f
ziglewis <ziglewis48@gmail.com>
2023-08-09 05:55:37 +0100

    added footer for social icons

--- src/content.js
+++ src/content.js
@@ -1,4 +1,5 @@
 
+import Footer from "./footer"
 
 const Content = () => {
   return (
@@ -7,6 +8,8 @@ const Content = () => {
             <p>React Developer</p>
             <h2>Rivers state, Nigeria.</h2>
             <p>(ziglewis48@gmail.com) </p>
+            <p> <Footer /> </p>
+            <p><i>Rebuilt with React! <a rel="noopner noreferrer" target="_blank" href="https://ziglewis.github.io/git_test5/Bcard/index.html">see initial version</a></i></p>
     </div>
   )
 }
---
+++ src/footer.js
@@ -0,0 +1,36 @@
+
+const Footer = () => {
+
+    return (
+        
+                <footer>
+ 
+                    <div class="social-icons">
+
+                            <a rel="noopener noreferrer"   target="_blank"  href="https://web.facebook.com/lewis.zigede/" >
+                            <i class="fab fa-facebook-f"></i>
+                            </a>
+                            
+                            <a rel="noopener noreferrer"   target="_blank"  href="https://twitter.com/lewis_zig/" > 
+                            <i class="fab fa-twitter"></i>
+                            </a>
+                            
+                        
+                            <a rel="noopener noreferrer"   target="_blank"  href="https://www.linkedin.com/in/zigede-lewis-11b387131/" > 
+                            <i class="fab fa-linkedin-in"></i>
+                            </a>
+
+                            <a  rel="noopener noreferrer"   target="_blank"  href="https://github.com/ziglewis" > 
+                                <i class="fab fa-github"></i>
+                            </a>
+                       
+                    </div>
+
+                 </footer> 
+
+
+    )
+
+}
+
+export default Footer
