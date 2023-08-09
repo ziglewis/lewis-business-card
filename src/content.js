3f5538b18e713771b3d6979f04c5f2d2876cadf3
ziglewis <ziglewis48@gmail.com>
2023-08-09 04:19:40 +0100

    built the function components

---
+++ src/content.js
@@ -0,0 +1,14 @@
+
+
+const Content = () => {
+  return (
+    <div className ="text">
+            <h1> Zigede Lewis Junior </h1>
+            <p>React Developer</p>
+            <h2>Rivers state, Nigeria.</h2>
+            <p>(ziglewis48@gmail.com) </p>
+    </div>
+  )
+}
+
+export default Content
---
+++ src/profileImage.js
@@ -0,0 +1,20 @@
+
+import Photo from './lewisz.jpg'    
+// import profile from '../images/profile.jpg';
+
+const ProfileImage = () => {
+
+    return ( 
+    
+    <span>
+            <img 
+            class="images"
+            src={Photo}
+            alt="Smiling Lewis with a red and black checkers blazer and white turtle neck t-shirt"> 
+            </img>
+    </span>
+    )
+
+}
+
+export default ProfileImage
